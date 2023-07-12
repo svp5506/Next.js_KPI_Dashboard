61f78bccdba951263d321f179169e788c4fb3a61
SP <svp5506@outlook.com>
2023-07-11 18:55:38 -0600

    Header and Stats Section Created

---
+++ app/header.tsx
@@ -0,0 +1,82 @@
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Stats from './stats'
+
+const user = {
+    name: 'Tom Cook',
+    email: 'tom@example.com',
+    imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+    { name: 'Dashboard', href: '#', current: true },
+    { name: 'Tables', href: '#', current: false },
+    { name: 'Documentation', href: '#', current: false },
+]
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Header() {
+    return (
+        <>
+            <div className="min-h-full bg-gray-800">
+                <div className="pb-24">
+                    <Disclosure as="nav" className="bg-gray-800">
+                        {({ open }) => (
+                            <>
+                                <div className="mx-auto sm:px-6 lg:px-">
+                                    <div className="border-b border-gray-700">
+                                        <div className="flex h-16 items-center justify-between px-4 sm:px-0">
+                                            <div className="flex items-center">
+                                                <div className="flex-shrink-0">
+                                                    <img
+                                                        className="h-8 w-8"
+                                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                                        alt="Your Company"
+                                                    />
+                                                </div>
+                                                <div className="hidden md:block">
+                                                    <div className="ml-10 flex items-baseline space-x-4">
+                                                        {navigation.map((item) => (
+                                                            <a
+                                                                key={item.name}
+                                                                href={item.href}
+                                                                className={classNames(
+                                                                    item.current
+                                                                        ? 'bg-gray-900 text-white'
+                                                                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                                                )}
+                                                                aria-current={item.current ? 'page' : undefined}
+                                                            >
+                                                                {item.name}
+                                                            </a>
+                                                        ))}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                    </Disclosure>
+                    <header className="py-10">
+                        <div className="mx-auto px-6 lg:px-24 sm:px-6.text-center ">
+                            <h1 className="text-3xl font-bold tracking-tight text-white">Dashboard</h1>
+                        </div>
+                    </header>
+                </div>
+
+                <main className="-mt-32 min-h-full">
+                    <div>
+                        <Stats></Stats>
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+}
---
+++ app/stats.tsx
@@ -0,0 +1,63 @@
+import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid'
+import { CursorArrowRaysIcon, EnvelopeOpenIcon, UsersIcon } from '@heroicons/react/24/outline'
+
+const stats = [
+    { id: 1, name: 'Total Subscribers', stat: '71,897', icon: UsersIcon, change: '122', changeType: 'increase' },
+    { id: 2, name: 'Avg. Open Rate', stat: '58.16%', icon: EnvelopeOpenIcon, change: '5.4%', changeType: 'increase' },
+    { id: 3, name: 'Avg. Click Rate', stat: '24.57%', icon: CursorArrowRaysIcon, change: '3.2%', changeType: 'decrease' },
+    { id: 4, name: 'Avg. Click Rate', stat: '24.57%', icon: CursorArrowRaysIcon, change: '3.2%', changeType: 'decrease' },
+]
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Stats() {
+    return (
+        <div className='px-24'>
+            <h3 className="text-base font-semibold leading-6 text-white">Last 30 days</h3>
+
+            <dl className="mt-5 grid grid-cols-1 gap-12 sm:grid-cols-2 lg:grid-cols-4">
+                {stats.map((item) => (
+                    <div
+                        key={item.id}
+                        className="relative overflow-hidden rounded-lg bg-gray-900 px-4 pb-0 pt-5 shadow sm:px-6 sm:pt-6"
+                    >
+                        <dt>
+                            <div className="absolute rounded-md bg-indigo-500 p-3">
+                                <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                            </div>
+                            <p className="ml-16 truncate text-sm font-medium text-white">{item.name}</p>
+                        </dt>
+                        <dd className="ml-16 flex items-baseline pb-6 sm:pb-7">
+                            <p className="text-2xl font-semibold text-white">{item.stat}</p>
+                            <div
+                                className={classNames(
+                                    item.changeType === 'increase' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800',
+                                    'inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0 ml-auto min-w-fit"'
+                                )}
+                            >
+                                {item.changeType === 'increase' ? (
+                                    <ArrowUpIcon
+                                        className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
+                                        aria-hidden="true"
+                                    />
+                                ) : (
+                                    <ArrowDownIcon
+                                        className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
+                                        aria-hidden="true"
+                                    />
+                                )}
+
+                                <span className="sr-only"> {item.changeType === 'increase' ? 'Increased' : 'Decreased'} by </span>
+                                {item.change}
+                            </div>
+
+                        </dd>
+                    </div>
+                ))}
+            </dl>
+        </div>
+    )
+}
+
