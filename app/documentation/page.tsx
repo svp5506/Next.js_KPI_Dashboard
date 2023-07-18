765ed052c6fb5cea76b6d5a3e2c8d02c64c82a26
SP <svp5506@outlook.com>
2023-07-17 18:43:22 -0600

    Documentation page.tsx Fix

---
+++ app/documentation/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Flex } from "@tremor/react";
+import MainChart from "../mainChart";
+
+export default function Documentation() {
+    return (
+        <main>
+            <Flex>
+                <div className="mx-auto my-4 w-9/12">
+                    <MainChart></MainChart>
+                </div>
+            </Flex>
+        </main>
+    )
+}
