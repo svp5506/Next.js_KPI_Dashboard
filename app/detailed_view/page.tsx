c9af2b2fd11a8654b6e859efcf9b384badf9c1f3
SP <svp5506@outlook.com>
2023-07-17 20:15:06 -0600

    Android Table

--- app/detailed_view/page.tsx
+++ app/detailed_view/page.tsx
@@ -1,15 +1,13 @@
 "use client";
 import { Flex } from "@tremor/react";
-import MainChart from "../mainChart";
+import AndroidTable from "./androidTable";
 
 export default function DetailedView() {
     return (
         <main>
-            <Flex>
-                <div className="mx-auto my-4 w-9/12">
-                    <MainChart></MainChart>
-                </div>
-            </Flex>
+            <div className="mx-auto my-4 w-9/12">
+                <AndroidTable></AndroidTable>
+            </div>
         </main>
     )
 }
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-500 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -39,22 +39,20 @@ export default function Navbar() {
                     alt="Sus"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4 mt-3">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-blue-800 text-white' : 'text-gray-700 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
+                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'border-b-2 border-blue-500 text-gray-800' : 'text-gray-500 hover:border-gray-400 hover:border-b-2 hover:text-gray-800',
+                        'px-1 pt-1 text-sm font-medium inline-flex items-center'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -70,7 +68,7 @@ export default function Navbar() {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-blue-800 text-white' : 'text-gray-700 hover:bg-gray-500 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
